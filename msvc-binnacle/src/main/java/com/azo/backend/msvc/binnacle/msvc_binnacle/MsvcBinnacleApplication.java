4be38074fcbff3f8cefe45e92cb4518c7ee0da33
Paulo V <paulos_50@hotmail.com>
2024-09-13 15:01:08 -0500

    update_binnacle

---
+++ msvc-binnacle/src/main/java/com/azo/backend/msvc/binnacle/msvc_binnacle/MsvcBinnacleApplication.java
@@ -0,0 +1,13 @@
+package com.azo.backend.msvc.binnacle.msvc_binnacle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcBinnacleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcBinnacleApplication.class, args);
+	}
+
+}
---
+++ msvc-binnacle/src/test/java/com/azo/backend/msvc/binnacle/msvc_binnacle/MsvcBinnacleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azo.backend.msvc.binnacle.msvc_binnacle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcBinnacleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
