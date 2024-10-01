f10abc8b1fbbb61010710d86da88549e76e30d06
Paulo V <paulos_50@hotmail.com>
2024-10-01 16:55:42 -0500

    update Factory method

--- msvc-binnacle/src/main/java/com/azo/backend/msvc/binnacle/msvc_binnacle/services/CadastralRecordServiceImpl.java
+++ msvc-binnacle/src/main/java/com/azo/backend/msvc/binnacle/msvc_binnacle/services/CadastralRecordServiceImpl.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.azo.backend.msvc.binnacle.msvc_binnacle.clients.UserClientRest;
@@ -18,6 +19,7 @@ import com.azo.backend.msvc.binnacle.msvc_binnacle.repositories.CadastralRecordR
 
 import feign.FeignException;
 
+@Service
 public class CadastralRecordServiceImpl implements CadastralRecordService{
 
   @Autowired
