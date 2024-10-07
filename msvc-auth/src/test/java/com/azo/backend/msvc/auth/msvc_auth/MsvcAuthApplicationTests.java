a964900ae2bdb19d8aa80fb3adc328f78f217917
Paulo V <paulos_50@hotmail.com>
2024-10-07 16:49:02 -0500

    create msvc-auth

---
+++ msvc-auth/src/test/java/com/azo/backend/msvc/auth/msvc_auth/MsvcAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azo.backend.msvc.auth.msvc_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
